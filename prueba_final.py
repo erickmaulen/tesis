e29dd41f612a2e165cebc0ec8fa8df7f2db2d1dd
erickmaulen99 <erick@conomyhq.com>
2022-06-15 17:06:54 -0400

    Diccionario accion, movimiento

--- prueba_final.py
+++ prueba_final.py
@@ -13,15 +13,6 @@ from math import dist
 from sewar.full_ref import mse, rmse, psnr, uqi, ssim, ergas, scc, rase, sam, msssim, vifp
 
 
-# def mse (imageA,imageB):
-#   shapes = (10,10)
-#   if shapes == imageA.shape and shapes == imageB.shape:   
-#     err = np.sum((imageA.astype("float") - imageB.astype("float")) ** 2)
-#     err /= float(np.mat(imageA.shape[0]) * np.mat(imageA.shape[1]))
-#     print("hola")
-#     return err
-#   else:
-#         return 101
 
 # Aca se setean los parametros del detector de Blobs
 params = cv2.SimpleBlobDetector_Params()
@@ -57,14 +48,13 @@ step = 0
 env.reset()
 accionesAgent = list()
 while not done and step < 10:
-    action = env.action_space.sample()
-    accionesAgent.append(action)
-    state_next, reward, done, info = env.step(action)
-    image = state_next
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-    images.append(image)
-
+      action = env.action_space.sample()
+      accionesAgent.append(action)
+      state_next, reward, done, info = env.step(action)
+      image = state_next
+      cv2.waitKey(0)
+      cv2.destroyAllWindows()
+      images.append(image)
 
 #Aca se obtiene el alto y ancho debido a que se debera recortar la imagen final
 height, width = state_next.shape[0:2]
@@ -74,8 +64,8 @@ video = cv2.VideoWriter('Ms-Pacman-v0.wmv',cv2.VideoWriter_fourcc(*'mp4v'),25,(w
 videoblob = cv2.VideoWriter('Ms-Pacman-v0Blob.wmv',cv2.VideoWriter_fourcc(*'mp4v'),25,(width,height-40))
 
 for i in range(len(images)):
-    #print(len(images))
-    video.write(images[i][0:height-40,0:width])
+      #print(len(images))
+      video.write(images[i][0:height-40,0:width])
 
 video.release()   
 
@@ -88,9 +78,9 @@ frameIds = cap.get(cv2.CAP_PROP_FRAME_COUNT) * np.random.uniform(size=25)
 
 frames = []
 for fid in frameIds:
-    cap.set(cv2.CAP_PROP_POS_FRAMES, fid)
-    ret, frame = cap.read()
-    frames.append(frame)
+      cap.set(cv2.CAP_PROP_POS_FRAMES, fid)
+      ret, frame = cap.read()
+      frames.append(frame)
 
 
 # Calcular la mediana a lo largo del eje temporal
@@ -108,163 +98,153 @@ object_detector = cv2.createBackgroundSubtractorMOG2()
 k = 0
 flag = True
 posiblesObjects = []
-
-while(ret):
-  #Aca solo ocupo el k para ver en que frame me encuentro
-  k+=1
-  # print(k)
-
-  # Leer el frame
-  ret, frame = cap.read()
-  if not ret:
-    print("GAMEOVER")
-    break
-  # Convertir el actual frame en escala de grises
-  frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-  # Se calcula la diferencia entre el frame actual y la media(fondo)
-  dframe = cv2.absdiff(frame, grayMedianFrame)
-  # Treshold a binario
-  th, dframe = cv2.threshold(dframe, 30, 255, cv2.THRESH_BINARY)
-  
-  #Deteccion de objetos
-  mask = object_detector.apply(dframe)
-  
-  _, mask = cv2.threshold(mask,254,255, cv2.THRESH_BINARY)
-  #contours, _ = cv2.findContours(mask,cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-  blob = mask
-  blobs.append(blob)
-  #Detectar los posibles Blobs (agrupacion de pixeles con similitudes)
-  keypoints = detector.detect(mask)
-  #Se detectan los blobs con circulos rojos 
-  im_with_keypoints = cv2.drawKeypoints(mask, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-  #Si hay frame
-  if ret == True:
-    gauss = cv2.GaussianBlur(mask, (5,5), 0)
-    canny = cv2.Canny(gauss, 50, 150)
-    (contornos,_) = cv2.findContours(canny.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    cv2.drawContours(frame,contornos,-1,(0,0,255), 2)   
-    for key in keypoints:
-      x = key.pt[0]
-      y = key.pt[1]
- 
-    centers = []
-    lastPosition = []
-    objetos = []
-    flag = True
-    dicObjects = {
+lastPosition = []
+objetos = list()
+actions = list()
+move = list()
+dicObjects = {
       "Objetos" : posiblesObjects,
       "Posiciones" :lastPosition
-    }
-
-
-    actions = list()
-    move = list()
-
-    dictionaryAction = {
+}
+dictionaryAction = {
       "Accion" : accionesAgent,
       "Move" : move
-    }
-    r = 5
-    #Ciclo para dibujar los circulos en cada objeto
-    for i in range(0,len(contornos)):
-      (rx, ry), radio = cv2.minEnclosingCircle(contornos[i])
-      center = (int(rx),int(ry))
-      centers.append(center)
-      radius = int(r)
-      dframe = cv2.circle(dframe,center,9,(255,0,0),0)
-      cv2.imshow('dfa',dframe)
-      
-    #Ciclo de la magia
-    for i in range(0,len(centers)):        
-      x = centers[i][0]
-      y = centers[i][1]
-      rectX = (x - r) 
-      rectY = (y - r)
-      if(rectX < 0):
-          rectX = 0
-      #Oldobject es un objeto , lo que hace es cortar segun el 
-      #centro estimado del objeto y esa matriz asignarla a una variable
-      objeto = dframe[rectY:(y+r),rectX:(x+r)]
-      #Cuando captura un posible objeto pero un
-      #un frame ya no esta, este if evita ese problema
-      if objeto.size == 0:
-        continue
-      else:
-        #Posicion de un objeto
-        position = x,y
-        if len(objetos) == 0:
-            # flag = False
-            objetos.append(list())
-            objetos[i].append(objeto)
+}
 
-            lastPosition.append(list())
-            lastPosition[i].append(position)
-        else:
-              if len(objetos) == i:
-                    objetos.append(list())
-                    objetos[i].append(objeto)
+while(ret):
+      #Aca solo ocupo el k para ver en que frame me encuentro
+      k+=1
+      # print(k)
 
-                    lastPosition.append(list())
-                    lastPosition[i].append(position)
+      # Leer el frame
+      ret, frame = cap.read()
+      if not ret:
+            print("GAMEOVER")
+            break
+      # Convertir el actual frame en escala de grises
+      frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-              else:      
-                for j in range(len(objetos)):
-                      positionRelative = lastPosition[i][-1] 
-                      if dist(positionRelative, position) == 1 :
-                            if isinstance(objetos[j], list) or isinstance(objetos[j], np.ndarray):
+      # Se calcula la diferencia entre el frame actual y la media(fondo)
+      dframe = cv2.absdiff(frame, grayMedianFrame)
+      # Treshold a binario
+      th, dframe = cv2.threshold(dframe, 30, 255, cv2.THRESH_BINARY)
+      #Deteccion de objetos
+      mask = object_detector.apply(dframe)
+      _, mask = cv2.threshold(mask,254,255, cv2.THRESH_BINARY)
+      #contours, _ = cv2.findContours(mask,cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+      blob = mask
+      blobs.append(blob)
+      #Detectar los posibles Blobs (agrupacion de pixeles con similitudes)
+      keypoints = detector.detect(mask)
+      #Se detectan los blobs con circulos rojos 
+      im_with_keypoints = cv2.drawKeypoints(mask, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+      #Si hay frame
+
+      if ret == True:
+            gauss = cv2.GaussianBlur(mask, (5,5), 0)
+            canny = cv2.Canny(gauss, 50, 150)
+            (contornos,_) = cv2.findContours(canny.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            cv2.drawContours(frame,contornos,-1,(0,0,255), 2)   
+            for key in keypoints:
+                  x = key.pt[0]
+                  y = key.pt[1]
+            centers = []
+            r = 5
+            #Ciclo para dibujar los circulos en cada objeto
+            for i in range(0,len(contornos)):
+                  (rx, ry), radio = cv2.minEnclosingCircle(contornos[i])
+                  center = (int(rx),int(ry))
+                  centers.append(center)
+                  radius = int(r)
+                  dframe = cv2.circle(dframe,center,9,(255,0,0),0)
+                  cv2.imshow('dfa',dframe)
+            #Ciclo de la magia
+            for i in range(0,len(centers)):      
+                  x = centers[i][0]
+                  y = centers[i][1]
+                  rectX = (x - r) 
+                  rectY = (y - r)
+                  if(rectX < 0):
+                        rectX = 0
+                  #Oldobject es un objeto , lo que hace es cortar segun el 
+                  #centro estimado del objeto y esa matriz asignarla a una variable
+                  objeto = dframe[rectY:(y+r),rectX:(x+r)]
+                  #Cuando captura un posible objeto pero un
+                  #un frame ya no esta, este if evita ese problema
+                  if objeto.size == 0:
+                        continue
+                  else:
+                  #Posicion de un objeto
+                        position = x,y
+                        if len(objetos) == 0:
+                              objetos.append(list())
                               objetos[i].append(objeto)
+                              lastPosition.append(list())
                               lastPosition[i].append(position)
-                              if (positionRelative[0] > position[0]):
-                                    move.append(3)
-                              elif (positionRelative[0] < position[0]):
-                                    move.append(2)
-                              elif (positionRelative[1] > position[1]):
-                                    move.append(4)
-                              elif (positionRelative[1] < position[1]):
-                                    move.append(1)
-                              break
-                            else:
-                                  objetos.append(list())
-                                  objetos[i].append(objeto)
-                                  lastPosition.append(list())
-                                  lastPosition[i].append(position)
-                                  if (positionRelative[0] > position[0]):
-                                        move.append(3)
-                                  elif (positionRelative[0] < position[0]):
-                                        move.append(2)
-                                  elif (positionRelative[1] > position[1]):
-                                        move.append(4)
-                                  elif (positionRelative[1] < position[1]):
-                                        move.append(1)
-                                  break
-
-                      objetos.append(list())
-                      objetos[i].append(objeto)
-
-                      lastPosition.append(list())
-                      lastPosition[i].append(position)
-            
-                              
-       
-                          
-                                       
-    flag = False    
-    videoblob.write(im_with_keypoints)
-    
-    #Aca se muestra frame a frame como va jugando la IA y detectando los diferentes objetos
-    cv2.imshow('dframe', im_with_keypoints)
-    
-  else:
-      cap.release()
-      videoblob.release()
-      break
-  
-  # Mostrar frame que ocupo el metodo de cv2 para la deteccion de objetos 
-  #cv2.imshow('frame', frame)
-  cv2.waitKey(20)
-
-
+                        else:
+                              if len(objetos) == i:
+                                    objetos.append(list())
+                                    objetos[i].append(objeto)
+                                    lastPosition.append(list())
+                                    lastPosition[i].append(position)
+                                    move.append(list())
+                              else:
+                                    for j in range(len(objetos)):
+                                          positionRelative = lastPosition[i][-1] 
+                                          if dist(positionRelative, position) == 1 :
+                                                if isinstance(objetos[i], list) or isinstance(objetos[i], np.ndarray):
+                                                      objetos[i].append(objeto)
+                                                      lastPosition[i].append(position)
+                                                      if isinstance(move[i], list) or isinstance(move[i], np.ndarray):
+                                                            if (positionRelative[0] > position[0]):
+                                                                  move[i].append(3)
+                                                            elif (positionRelative[0] < position[0]):
+                                                                  move[i].append(2)
+                                                            elif (positionRelative[1] > position[1]):
+                                                                  move[i].append(4)
+                                                            elif (positionRelative[1] < position[1]):
+                                                                  move[i].append(1)
+                                                            break
+                                                      else:
+                                                            move.append(list())
+                                                            if (positionRelative[0] > position[0]):
+                                                                  move[i].append(3)
+                                                            elif (positionRelative[0] < position[0]):
+                                                                  move[i].append(2)
+                                                            elif (positionRelative[1] > position[1]):
+                                                                  move[i].append(4)
+                                                            elif (positionRelative[1] < position[1]):
+                                                                  move[i].append(1)
+                                                            break
+                                                else:
+                                                      objetos.append(list())
+                                                      objetos[i].append(objeto)
+                                                      lastPosition.append(list())
+                                                      lastPosition[i].append(position)
+                                                      if (positionRelative[0] > position[0]):
+                                                            move.append(3)
+                                                      elif (positionRelative[0] < position[0]):
+                                                            move.append(2)
+                                                      elif (positionRelative[1] > position[1]):
+                                                            move.append(4)
+                                                      elif (positionRelative[1] < position[1]):
+                                                            move.append(1)
+                                                      break
+                                    objetos.append(list())
+                                    objetos[i].append(objeto)
+                                    lastPosition.append(list())
+                                    lastPosition[i].append(position)
+            videoblob.write(im_with_keypoints)
+            #Aca se muestra frame a frame como va jugando la IA y detectando los diferentes objetos
+            cv2.imshow('dframe', im_with_keypoints)
+      else:
+            cap.release()
+            videoblob.release()
+            break
+      # Mostrar frame que ocupo el metodo de cv2 para la deteccion de objetos 
+      #cv2.imshow('frame', frame)
+      cv2.waitKey(20)
+print(dictionaryAction)
 video.release()  
 cap.release()
 cv2.destroyAllWindows()
